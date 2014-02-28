4902c50c6e3c023ab17393a46aebac820d99bf80
jnozsc <jnozsc@gmail.com>
2014-02-27 19:06:50 -0500

    plusOne

---
+++ plusOne.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if (digits.size() < 1) return digits;
+        vector<int> result(digits.begin(), digits.end());
+        bool carry = true;
+        for (int i = result.size() - 1; i >= 0; i--) {
+            int tmp = result[i] + 1;
+            if (tmp == 10) {
+                result[i] = 0;
+                carry = true;
+            } else {
+                result[i] = tmp;
+                carry = false;
+            }
+            if (carry == false)break;
+        }
+        if (carry) {
+            result.insert(result.begin(), 1);
+        }
+        return result;
+    }
+};
