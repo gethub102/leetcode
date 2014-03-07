eb7917a81edfc83209dbd86d8f76138e953e3fcb
jnozsc <jnozsc@gmail.com>
2014-03-07 10:49:07 -0500

    new answer

---
+++ Reverse_Words_in_a_String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void reverseWords(string& s) {
+        if (s.empty())return;
+        stack<string> stringStack;
+        istringstream iss(s);
+        string token;
+		// split
+        while (getline(iss, token, ' ')) {
+            stringStack.push(token);
+        }
+        if (stringStack.empty())return;
+        s = "";
+        while (!stringStack.empty()) {
+			// still some space, handle it here
+            if (!stringStack.top().empty()) {
+                s += stringStack.top();
+                s += " ";
+            }
+            stringStack.pop();
+        }
+		// remove last space
+        s = s.substr(0, s.length() - 1);
+    }
+};
