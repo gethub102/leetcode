3603911cc583e41bae3280360649820f76b43eb2
jnozsc <jnozsc@gmail.com>
2014-03-11 13:27:51 -0400

    new answer

---
+++ Count_and_Say.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        int times = 1;
+        string result = "1";
+        while (times < n) {
+            vector<string> stringV;
+            vector<int> countV;
+            int index = 0;
+            while (index < result.length()) {
+                if (stringV.empty() || stringV.back() != result.substr(index, 1)) {
+                    stringV.push_back(result.substr(index, 1));
+                    countV.push_back(1);
+                } else {
+                    countV[countV.size() - 1] ++;
+                }
+                index++;
+            }
+            result = "";
+            while (!stringV.empty()) {
+                result = stringV.back() + result;
+                result = to_string(countV.back()) + result;
+                stringV.pop_back();
+                countV.pop_back();
+            }
+            times++;
+        }
+        return result;
+    }
+};
