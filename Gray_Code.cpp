4e64a5f1cf6126906e4eb63521cb404f7293c870
jnozsc <jnozsc@gmail.com>
2014-03-14 17:12:32 -0400

    new answer

---
+++ Gray_Code.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        int size = 1 << n;
+        for (int i = 0; i < size; ++i)
+            result.push_back((i >> 1)^i);
+        return result;
+    }
+};
