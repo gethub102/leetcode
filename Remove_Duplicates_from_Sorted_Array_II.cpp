aabaa05cd9c05048c10d2c626396eee5a0056020
jnozsc <jnozsc@gmail.com>
2014-03-14 20:06:49 -0400

    new answer

---
+++ Remove_Duplicates_from_Sorted_Array_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if (n <= 2) return n;
+        int occur = 1;
+        int index = 0;
+        for (int i = 1; i < n; i++) {
+            if (A[index] == A[i]) {
+                if (occur == 2) {
+                    continue;
+                }
+                occur++;
+            } else {
+                occur = 1 ;
+            }
+            A[++index] = A[i];
+        }
+        return index + 1;
+    }
+};
---
+++ Subsets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int>& S) {
+        sort(S.begin(), S.end());
+        vector<vector<int> > v(1);
+        for (int i = 0; i < S.size(); ++i) {
+            int j = v.size();
+            while (j > 0) {
+                j--;
+                v.push_back(v[j]);
+                v.back().push_back(S[i]);
+            }
+        }
+        return v;
+    }
+};
