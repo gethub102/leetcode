574bf229daa91bf3c46cd688d115676f3ea40dd2
jnozsc <jnozsc@gmail.com>
2014-03-15 19:27:11 -0400

    new answer

---
+++ Jump_Game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(int A[], int n) {
+        int maxCover = 0;
+        for (int start = 0; start <= maxCover && start < n; start++)         {
+            maxCover = max(maxCover, A[start] + start);
+            if (maxCover >= n - 1) return true;
+        }
+        return false;
+    }
+};
