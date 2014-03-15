565fc18482ad99de021cdd5056b2f7cf73f87aa3
jnozsc <jnozsc@gmail.com>
2014-03-15 15:31:04 -0400

    new answer

---
+++ Path_Sum_II.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int> > pathSum(TreeNode* root, int sum) {
+        vector<vector<int> > result;
+        vector<int> currentPath;
+        pathSumRecursion(result, currentPath, root, sum);
+        return result;
+    }
+
+    void pathSumRecursion(vector<vector<int> >& result, vector<int> currentPath, TreeNode* root, int sum) {
+        if (!root) return;
+        if (root->val == sum && !(root->left) && !(root->right)) {
+            currentPath.push_back(root->val);
+            result.push_back(currentPath);
+            return;
+        } else {
+            currentPath.push_back(root->val);
+            pathSumRecursion(result, currentPath, root->left, sum - root->val);
+            pathSumRecursion(result, currentPath, root->right, sum - root->val);
+        }
+    }
+};
