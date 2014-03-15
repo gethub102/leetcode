2dd66404f904c5787075ff9e6b23f0f6c7da7c4b
jnozsc <jnozsc@gmail.com>
2014-03-15 15:19:28 -0400

    new answer

---
+++ Trapping_Rain_Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(int A[], int n) {
+        int result = 0;
+        int i;
+        int left[n];
+        int right[n];
+        int sub[n];
+        left[0] = A[0];
+        for (i = 1; i < n; i++) {
+            left[i] = max(left[i - 1], A[i]);
+        }
+        right[n - 1] = A[n - 1];
+        sub[n - 1] = min(left[n - 1], right[n - 1]);
+        for (i = n - 2; i >= 0; i--) {
+            right[i] = max(right[i + 1], A[i]);
+            sub[i] = min(left[i], right[i]);
+            result += (sub[i] - A[i]);
+        }
+        return result;
+    }
+};
