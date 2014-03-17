486d2ae0b9d23b9210036e39b2b7d9db48f5346d
jnozsc <jnozsc@gmail.com>
2014-03-17 16:29:04 -0400

    new answer

---
+++ Anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> anagrams(vector<string>& strs) {
+        vector<string> result;
+        if (strs.size() <= 1) return result;
+        map<string, int> anagram;
+        for (int i = 0; i < strs.size(); i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            if (anagram.find(s) == anagram.end()) {
+                anagram.insert(make_pair(s, i));
+            } else {
+                if (anagram[s] >= 0) {
+                    result.push_back(strs[anagram[s]]);
+                    anagram[s] = -1;
+                }
+                result.push_back(strs[i]);
+            }
+        }
+        return result;
+    }
+};
