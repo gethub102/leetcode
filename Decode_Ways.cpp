bf7e70abc40fbede973c12309d5403f09fee2af9
jnozsc <jnozsc@gmail.com>
2014-03-18 11:27:49 -0400

    new answer

---
+++ Decode_Ways.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s.size() == 0)
+            return 0;
+        vector<int> ways(s.size() + 1, 1);
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '0')
+                ways[i] = 0;
+            else
+                ways[i] = ways[i + 1];
+            if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+                ways[i] += ways[i + 2];
+        }
+        return ways[0];
+    }
+};
