8a4ff12a8c2db6c8a2650a8e2f935cf5d7a78195
jnozsc <jnozsc@gmail.com>
2014-04-04 18:52:45 -0400

    rm one unneccessary loop

--- Candy.cpp
+++ Candy.cpp
@@ -1,24 +1,19 @@
 class Solution {
   public:
     int candy(vector<int>& ratings) {
-        if (ratings.size() <= 1) return ratings.size();
-        int i;
         vector<int> candy(ratings.size(), 1);
         int result = 0;
-        for (i = 1; i < ratings.size(); i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
-            }
-
+        for (int i = 1; i < ratings.size(); i++) {
+            if (ratings[i] > ratings[i - 1])candy[i] = candy[i - 1] + 1;
         }
-        for (i = ratings.size() - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
-                candy[i] = candy[i + 1] + 1;
-            }
-        }
-        for (i = 0; i < candy.size(); i++) {
+        result += candy[ratings.size() - 1];
+        for (int i = ratings.size() - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) candy[i] = candy[i + 1] + 1;
             result += candy[i];
         }
+        /*  for(int i =0;i<ratings.size();i++){
+        *        result += candy[i];
+          } */
         return result;
     }
 };
