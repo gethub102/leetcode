1698132529b23577c9fc19d3b3c1a8a557f8c28c
jnozsc <jnozsc@gmail.com>
2014-04-05 20:29:02 -0400

    just DFS, remember 001

--- Maximal_Rectangle.cpp
+++ Maximal_Rectangle.cpp
@@ -36,8 +36,9 @@ class Solution {
         height.push_back(0);
         int sum = 0;
         for (int i = 0; i < height.size(); i++) {
-            if (S.empty() || height[i] > height[S.top()]) S.push(i);
-            else {
+            if (S.empty() || height[i] > height[S.top()]) {
+                S.push(i);
+            } else {
                 int tmp = S.top();
                 S.pop();
                 sum = max(sum, height[tmp] * (S.empty() ? i : i - S.top() - 1));
---
+++ Restore_IP_Addresses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> results;
+        string oneSolution;
+        restoreIpAddressesDFS(results, oneSolution, s, 0);
+        return results;
+
+    }
+
+    void restoreIpAddressesDFS(vector<string>& results, string oneSolution, string s, int byte) {
+        if (byte == 4 && s.empty()) {
+            results.push_back(oneSolution);
+            return;
+        }
+        if (byte == 4 && !s.empty()) {
+            return;
+        }
+        for (int i = 1; i <= 3; i++) {
+            if (i <= s.length() && isValidIP(s.substr(0, i))) {
+                string old_oneSolution = oneSolution;
+                oneSolution = oneSolution.empty() ? s.substr(0, i) : oneSolution + "." + s.substr(0, i);
+                restoreIpAddressesDFS(results, oneSolution, s.substr(i), byte + 1);
+                oneSolution = old_oneSolution;
+            }
+        }
+    }
+
+    bool isValidIP(string s) {
+        if (s.length() > 1 && s[0] == '0') return false; // case 001,is false
+        int value = stoi(s);
+        return value >= 0 && value <= 255;
+    }
+};
