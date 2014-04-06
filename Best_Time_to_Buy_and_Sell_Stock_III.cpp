59872caf08c94ddd7bef1354e3a177cc25f33926
jnozsc <jnozsc@gmail.com>
2014-04-06 15:10:38 -0400

    1d dp, 2way scan

---
+++ Best_Time_to_Buy_and_Sell_Stock_III.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        vector<int> profitFromBeginToI(prices.size(), 0);
+        vector<int> profitFromIToEnd(prices.size(), 0);
+        getProfitFromBeginToI(prices, profitFromBeginToI);
+        getProfitFromIToEnd(prices, profitFromIToEnd);
+        int maxProfit = profitFromBeginToI[prices.size() - 1];
+        for (int i = 1; i < prices.size() - 1; i++) {
+            // 这里一定要错开加
+            maxProfit = max(maxProfit, profitFromBeginToI[i] + profitFromIToEnd[i + 1]);
+        }
+        return maxProfit;
+    }
+
+    // 从【0】到【i】的最大 profit
+    void getProfitFromBeginToI(vector<int>& prices, vector<int>& result) {
+        //vector<int> result;
+        //result.push_back(0); // result[0]==0;
+        int min_value = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.size(); i++) {
+            maxProfit = max(maxProfit, prices[i] - min_value);
+            min_value = min(min_value, prices[i]);
+            result[i] = maxProfit;
+        }
+        //return result;
+    }
+
+    // 从【i】到尾巴的最大 profit
+    void getProfitFromIToEnd(vector<int>& prices, vector<int>& result) {
+        //vector<int> result(prices.size(),0);
+        int max_value = prices.back();
+        int maxProfit = 0;
+        for (int i = prices.size() - 1; i >= 0; i--) {
+            maxProfit = max(maxProfit, max_value - prices[i]);
+            max_value = max(max_value, prices[i]);
+            result[i] = maxProfit;
+        }
+        // return result;
+    }
+};
