caa522a5cdb1b37eb7e2cd29bae95a06408116b5
jnozsc <jnozsc@gmail.com>
2014-04-12 14:17:47 -0400

    rewrite

--- Search_in_Rotated_Sorted_Array.cpp
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -1,14 +1,28 @@
-// should rewrite
 class Solution {
   public:
     int search(int A[], int n, int target) {
-        // spcial cases
-        if (n <= 0) return -1;
-        if (n == 1) return A[0] == target ? 0 : -1;
-        // general case
-        for (int i = 0; i < n; i++) {
-            if (A[i] == target) return i;
+        return   searchRecursion(A, 0, n - 1, target);
+    }
+
+    int searchRecursion(int A[], int begin, int end, int target) {
+        if (begin > end) return -1;
+        if (begin == end) return A[begin] == target ? begin : -1;
+        int mid = begin + (end - begin) / 2;
+        if (A[mid] == target) return mid;
+        if (A[begin] < A[mid]) {
+            if (A[begin] <= target && target <= A[mid]) {
+                return searchRecursion(A, begin, mid - 1, target);
+            } else {
+                return searchRecursion(A, mid + 1, end, target);
+            }
+        } else if (A[mid] < A[end]) {
+            if (A[mid] <= target && target <= A[end]) {
+                return searchRecursion(A, mid + 1, end, target);
+            } else {
+                return searchRecursion(A, begin, mid - 1, target);
+            }
+        } else { // a[begin] == a[mid], directly search right
+            return searchRecursion(A, mid + 1, end, target);
         }
-        return -1;
     }
 };
