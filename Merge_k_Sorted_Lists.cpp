5eee597aa939a0258014dc49f5b69feb039309d2
jnozsc <jnozsc@gmail.com>
2014-04-12 22:31:06 -0400

    remove tmp nodes

--- Merge_k_Sorted_Lists.cpp
+++ Merge_k_Sorted_Lists.cpp
@@ -23,35 +23,24 @@ class Solution {
             return mergeKLists(two);
         }
         // merge 2 lists
-        if (!lists[0] && !lists[1]) return NULL;
-        if (lists[0] && !lists[1]) return lists[0];
-        if (lists[1] && !lists[0]) return lists[1];
-        ListNode* result = new ListNode(0);
-        ListNode* newNode = new ListNode(0);
-        result->next = newNode;
         ListNode* first = lists[0];
         ListNode* second = lists[1];
+        if (!first && !second) return NULL;
+        if (first && !second) return first;
+        if (!first && second) return second;
+        ListNode* newNode = new ListNode(0);
+        ListNode* result = newNode;
         while (first && second) {
             if (first->val < second -> val) {
-                newNode -> next = new ListNode(first->val);
-                newNode = newNode->next;
+                newNode -> next = first;
                 first = first->next;
             } else {
-                newNode -> next = new ListNode(second->val);
-                newNode = newNode->next;
+                newNode -> next = second;
                 second = second->next;
             }
-        }
-        while (first) {
-            newNode -> next = new ListNode(first->val);
             newNode = newNode->next;
-            first = first->next;
         }
-        while (second) {
-            newNode -> next = new ListNode(second->val);
-            newNode = newNode->next;
-            second = second->next;
-        }
-        return result->next->next;
+        newNode->next = first ? first : second;
+        return result->next;
     }
 };
