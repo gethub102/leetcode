ad487df19518abcceb8eb3b524a3a49f8ca14642
jnozsc <jnozsc@gmail.com>
2014-04-19 15:44:55 -0400

    rewrite

--- Subsets.cpp
+++ Subsets.cpp
@@ -2,15 +2,15 @@ class Solution {
   public:
     vector<vector<int> > subsets(vector<int>& S) {
         sort(S.begin(), S.end());
-        vector<vector<int> > v(1);
-        for (int i = 0; i < S.size(); ++i) {
-            int j = v.size();
-            while (j > 0) {
-                j--;
-                v.push_back(v[j]);
-                v.back().push_back(S[i]);
+        vector<vector<int> > result(1); // one empty subset
+        for (int i = 0; i < S.size(); i++) {
+            int j_max = result.size();
+            for (int j = 0; j < j_max; j++) {
+                vector<int> new_element = result[j];
+                new_element.push_back(S[i]);
+                result.push_back(new_element);
             }
         }
-        return v;
+        return result;
     }
 };
--- Subsets_II.cpp
+++ Subsets_II.cpp
@@ -2,18 +2,19 @@ class Solution {
   public:
     vector<vector<int> > subsetsWithDup(vector<int>& S) {
         sort(S.begin(), S.end());
-        vector<vector<int> > v(1);
-        for (int i = 0; i < S.size(); ++i) {
-            int j = v.size();
-            while (j > 0) {
-                j--;
-                v.push_back(v[j]);
-                v.back().push_back(S[i]);
+        vector<vector<int> > result(1);
+        for (int i = 0; i < S.size(); i++) {
+            int j_max = result.size();
+            for (int j = 0; j < j_max; j++) {
+                vector <int> new_element = result[j];
+                new_element.push_back(S[i]);
+                // add a check
+                if (find(result.begin(), result.end(), new_element) == result.end()) result.push_back(new_element);
             }
         }
         // remove duplicate
-        sort(v.begin(), v.end());
-        v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-        return v;
+        // sort(v.begin(), v.end());
+        // v.resize( distance(v.begin(), unique(v.begin(), v.end())) );
+        return result;
     }
 };
