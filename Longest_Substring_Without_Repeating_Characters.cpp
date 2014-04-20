b7843c6793aadf74232e675ccc30f5ef4634a541
jnozsc <jnozsc@gmail.com>
2014-04-20 14:13:30 -0400

    understand

--- Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -2,20 +2,20 @@ class Solution {
   public:
     int lengthOfLongestSubstring(string s) {
         if (s.empty()) return 0;
-        int checker = 0, val = 0, max = 0, j = 0, count = 0;
-        for (int i = 0; i < s.size() && j < s.size(); i++) {
-            j = i;
-            while (j < s.size()) {
-                val = s[j] - 'a';
-                if ((checker & (1 << val)) > 0) break;
-                checker |= 1 << val;
-                j++;
-                count++;
+        int i = 0, len = 0, n = s.length();
+        set<char> myset ;
+        for (int j = 0; j < n; j++) { // 结尾元素
+            if (myset.find(s[j]) != myset.end()) { //found dup
+                len = max(len, (j - 1) - i + 1);
+                while (i < n && s[i] != s[j]) { // remove abc from abcd123456d
+                    myset.erase(s[i]);
+                    i++;
+                }
+                i++; // remove d
+            } else {
+                myset.insert(s[j]);
             }
-            if (count > max) max = count;
-            checker = 0;
-            count = 0;
         }
-        return max;
+        return max(len, n - i);
     }
 };
