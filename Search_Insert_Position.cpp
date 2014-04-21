ec724ba05c79abdee8b89bb5ad2b06822b7d0a16
jnozsc <jnozsc@gmail.com>
2014-04-20 22:09:08 -0400

    previous total wrong

--- Search_Insert_Position.cpp
+++ Search_Insert_Position.cpp
@@ -1,10 +1,18 @@
 class Solution {
   public:
     int searchInsert(int A[], int n, int target) {
-        if (n == 0) return 0;
-        for (int i = 0; i < n; i++) {
-            if (target <= A[i]) return i;
+        int start = 0, end = n - 1;
+        while (start <= end)  {
+            int mid = (start + end) / 2;
+            if (A[mid] == target) return mid;
+            if (mid > start && A[mid] > target && A[mid - 1] < target) return mid;
+            if (A[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
         }
-        return n;
+        return start;
+
     }
 };
