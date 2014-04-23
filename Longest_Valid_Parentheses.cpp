235e96808e46edf76959c3203b81522fa8d43f98
jnozsc <jnozsc@gmail.com>
2014-04-23 18:43:23 -0400

    change varables

--- Longest_Valid_Parentheses.cpp
+++ Longest_Valid_Parentheses.cpp
@@ -1,17 +1,17 @@
 class Solution {
   public:
     int longestValidParentheses(string s) {
+
         if (s.length() < 2) return 0;
         int maxLen = 0;
         int dp[s.length()];
-        // dp[i] means a string begins with a char as s[i], if valid, how many, if not valid, zero
+        // dp[i] means a
         dp[s.length() - 1] = 0; // or use memset(dp, 0, sizeof(dp));
         for (int i = s.length() - 2; i >= 0; i--) {
             if (s[i] == ')') {
-                // invalid
                 dp[i] = 0;
             } else {
-                int j = i + 1 + dp[i + 1];
+                int j = i + dp[i + 1] + 1;
                 if (j < s.length() && s[j] == ')') {
                     dp[i] = dp[i + 1] + 2;
                     if (j + 1 < s.length()) dp[i] += dp[j + 1];
