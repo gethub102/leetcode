ec4e7d17d7f04cc46e3fbf46a55a650187f89aeb
jnozsc <jnozsc@gmail.com>
2014-04-26 18:04:23 -0400

    easy to understand

--- Symmetric_Tree.cpp
+++ Symmetric_Tree.cpp
@@ -10,19 +10,16 @@
 class Solution {
   public:
     bool isSymmetric(TreeNode* root) {
-        if (root == NULL) return true;
-        return isSym(root->left, root->right);
+        if (!root) return true;
+        return isSymmetricRecursion(root->left, root->right);
     }
-    bool isSym(TreeNode* left, TreeNode* right) {
-        if (left == NULL)
-            return right == NULL;
-        if (right == NULL)
-            return left == NULL;
-        if (left->val != right->val)
+    bool isSymmetricRecursion(TreeNode* left, TreeNode* right)  {
+        if (!left && !right) {
+            return true;
+        }
+        if (!left || !right) {
             return false;
-        if (!isSym(left->left, right->right))          return false;
-        if (!isSym(left->right, right->left))
-            return false;
-        return true;
+        }
+        return left->val == right->val && isSymmetricRecursion(left->left, right->right) && isSymmetricRecursion(left->right, right->left) ;
     }
 };
