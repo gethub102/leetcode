f0b65e4bdfa9b353a327c2618921d6dc15317ae9
jnozsc <jnozsc@gmail.com>
2014-05-03 00:35:28 -0400

    change style

--- Valid_Sudoku.cpp
+++ Valid_Sudoku.cpp
@@ -5,7 +5,9 @@ class Solution {
         vector<vector<char> > columns(9);
         vector<vector<char> > smallBlock(9);
         for (int i = 0; i < board.size(); i++) {
-            if (isValidVector(board[i]) == false) return false;
+            if (isValidVector(board[i]) == false) {
+                return false;
+            }
             for (int j = 0; j < board[0].size(); j++) {
                 // each columns
                 columns[j].push_back(board[i][j]);
@@ -14,15 +16,21 @@ class Solution {
             }
         }
         for (int k = 0; k < 9; k++) {
-            if (isValidVector(columns[k]) == false) return false;
-            if (isValidVector(smallBlock[k]) == false) return false;
+            if (isValidVector(columns[k]) == false) {
+                return false;
+            }
+            if (isValidVector(smallBlock[k]) == false) {
+                return false;
+            }
         }
         return true;
     }
 
     bool isValidVector(vector<char> myVector) {
         for (int i = 1; i < 9; i++) {
-            if (isdigit(myVector[i]) && find(myVector.begin(), myVector.begin() + i, myVector[i]) != myVector.begin() + i) return false;
+            if (isdigit(myVector[i]) && find(myVector.begin(), myVector.begin() + i, myVector[i]) != myVector.begin() + i) {
+                return false;
+            }
         }
         return true;
     }
