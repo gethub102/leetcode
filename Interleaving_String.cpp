242cca61daa225d67aa0e51e508a45b832de3cd2
jnozsc <jnozsc@gmail.com>
2014-05-04 15:35:32 -0400

    change dp

--- Interleaving_String.cpp
+++ Interleaving_String.cpp
@@ -1,52 +1,33 @@
 class Solution {
   public:
     bool isInterleave(string s1, string s2, string s3) {
-        // Find lengths of the two strings
-        int M = s1.length(), N = s2.length();
+        int n1 = s1.size();
+        int n2 = s2.size();
+        vector<vector<bool> > A(n1 + 1, vector<bool>(n2 + 1, false));
+        if (n1 + n2 != s3.size()) {
+            return false;
+        }
+        if (s1.empty() && s2.empty() && s3.empty()) {
+            return true;
+        }
 
-        // Let us create a 2D table to store solutions of
-        // subproblems.  C[i][j] will be true if C[0..i+j-1]
-        // is an interleaving of A[0..i-1] and B[0..j-1].
-        bool IL[M + 1][N + 1];
-        memset(IL, 0, sizeof(IL)); // Initialize all values as false.
-
-        // C can be an interleaving of A and B only of sum
-        // of lengths of A & B is equal to length of C.
-        if ((M + N) != s3.length()) return false;
-
-        // Process all characters of A and B
-        for (int i = 0; i <= M; ++i) {
-            for (int j = 0; j <= N; ++j) {
-                // two empty strings have an empty string
-                // as interleaving
-                if (i == 0 && j == 0) {
-                    IL[i][j] = true;
-
-                    // A is empty
-                } else if (i == 0 && s2[j - 1] == s3[j - 1]) {
-                    IL[i][j] = IL[i][j - 1];
-
-                    // B is empty
-                } else if (j == 0 && s1[i - 1] == s3[i - 1]) {
-                    IL[i][j] = IL[i - 1][j];
-
-                    // Current character of C matches with current character of A,
-                    // but doesn't match with current character of B
-                } else if (s1[i - 1] == s3[i + j - 1] && s2[j - 1] != s3[i + j - 1]) {
-                    IL[i][j] = IL[i - 1][j];
-
-                    // Current character of C matches with current character of B,
-                    // but doesn't match with current character of A
-                } else if (s1[i - 1] != s3[i + j - 1] && s2[j - 1] == s3[i + j - 1]) {
-                    IL[i][j] = IL[i][j - 1];
-
-                    // Current character of C matches with that of both A and B
-                } else if (s1[i - 1] == s3[i + j - 1] && s2[j - 1] == s3[i + j - 1]) {
-                    IL[i][j] = (IL[i - 1][j] || IL[i][j - 1]) ;
-                }
+        A[0][0] = true;
+        for (int i = 1; i <= n1; i++) {
+            if (s1[i - 1] == s3[i - 1] && A[i - 1][0]) {
+                A[i][0] = true;
+            }
+        }
+        for (int i = 1; i <= n2; i++) {
+            if (s2[i - 1] == s3[i - 1] && A[0][i - 1]) {
+                A[0][i] = true;
             }
         }
 
-        return IL[M][N];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                A[i][j] = (A[i][j - 1] && (s2[j - 1] == s3[i + j - 1])) || (A[i - 1][j] && (s1[i - 1] == s3[i + j - 1]));
+            }
+        }
+        return A[n1][n2];
     }
 };
