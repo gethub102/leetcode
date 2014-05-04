ec33e493f505abfa5a51ff51b08f7d7414b5a5c5
jnozsc <jnozsc@gmail.com>
2014-05-04 14:31:55 -0400

    use dp

--- Jump_Game.cpp
+++ Jump_Game.cpp
@@ -1,6 +1,25 @@
 class Solution {
   public:
     bool canJump(int A[], int n) {
+        if (n == 0) return false;
+        if (n == 1) return true;
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 0 ; j--) {
+                if (dp[j] == true && A[j] >= i - j) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+/*
+class Solution {
+  public:
+    bool canJump(int A[], int n) {
         int maxCover = 0;
         for (int start = 0; start <= maxCover && start < n; start++)         {
             maxCover = max(maxCover, A[start] + start);
@@ -9,3 +28,4 @@ class Solution {
         return false;
     }
 };
+*/
