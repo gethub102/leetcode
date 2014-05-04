e7ed38581283e22a3b04bbcf74f011f236fde4a0
jnozsc <jnozsc@gmail.com>
2014-05-04 13:37:56 -0400

    change style

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,18 +1,19 @@
 class Solution {
   public:
     int minimumTotal(vector<vector<int> >& triangle) {
-        if (triangle.size() == 0) return 0;
+        if (triangle.size() == 0) {
+            return 0;
+        }
         vector<int> dp(triangle.size(), triangle[0][0]);
         for (int i = 1; i < triangle.size(); i++) {
             dp[i] = dp[i - 1] + triangle[i][i];
-            for (int j = i - 1; j >= 1; j--) {
+            for (int j = i - 1; j > 0; j--) {
                 dp[j] = triangle[i][j] + min(dp[j], dp[j - 1]);
             }
             dp[0] = dp[0] + triangle[i][0];
-
         }
         int result = INT_MAX;
-        for (int k = 0; k < triangle.size(); k++) {
+        for (int k = 0; k < dp.size(); k++) {
             result = min(result, dp[k]);
         }
         return result;
