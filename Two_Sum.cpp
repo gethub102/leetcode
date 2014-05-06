8cb918d2a60151d501c3412014a10f9e69631188
jnozsc <jnozsc@gmail.com>
2014-05-05 21:35:29 -0400

    rewrite

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -2,26 +2,20 @@ class Solution {
   public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         vector<int> result;
-        if (numbers.size() <= 1)  {
+        if (numbers.size() < 2) {
             return result;
         }
-        if (numbers.size() == 2) {
-            if (numbers[0] + numbers[1] == target) {
-                result.push_back(0);
-                result.push_back(1);
-            }
-            return result;
-        }
-        // general case;
-        map<int, int> mymap; // keep <numbers[i], i>
+        map<int, int> dict;
         for (int i = 0; i < numbers.size(); i++) {
-            if (mymap.find(target - numbers[i]) != mymap.end()) {
-                result.push_back(mymap[target - numbers[i]] + 1);
+            if (dict.find(target - numbers[i]) != dict.end()) {
+                result.push_back(dict[target - numbers[i]] + 1);
                 result.push_back(i + 1);
                 return result;
             } else {
-                mymap[numbers[i]] = i;
+                dict[numbers[i]] = i;
             }
         }
+        // no pair
+        return result;
     }
 };
