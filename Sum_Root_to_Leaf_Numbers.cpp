703e9aea12c122f39f9bcfac70d281395f20b696
jnozsc <jnozsc@gmail.com>
2014-05-08 15:33:18 -0400

    rewrite

--- Sum_Root_to_Leaf_Numbers.cpp
+++ Sum_Root_to_Leaf_Numbers.cpp
@@ -10,17 +10,16 @@
 class Solution {
   public:
     int sumNumbers(TreeNode* root) {
-        if (!root) return 0;
-        if (!root->left && !root->right) return root->val;
-        int result = 0;
-        if (root->left) {
-            root->left->val = root->left->val + 10 * (root->val);
-            result += sumNumbers(root->left);
+        return sumNumbersRecursion(root, 0) ;
+    }
+
+    int sumNumbersRecursion(TreeNode* root, int parent) {
+        if (!root) {
+            return 0;
         }
-        if (root->right) {
-            root->right->val = root->right->val + 10 * (root->val);
-            result += sumNumbers(root->right);
+        if (!root->left && !root->right) {
+            return parent + root->val;
         }
-        return result;
+        return sumNumbersRecursion(root->left, 10 * (parent + root->val)) + sumNumbersRecursion(root->right, 10 * (parent + root->val));
     }
 };
