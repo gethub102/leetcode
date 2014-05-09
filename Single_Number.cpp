79a31c42e29828a5e0491d0d6d70379e5c132ad5
jnozsc <jnozsc@gmail.com>
2014-05-09 12:30:00 -0400

    more solution

--- Single_Number.cpp
+++ Single_Number.cpp
@@ -1,6 +1,16 @@
 class Solution {
   public:
     int singleNumber(int A[], int n) {
+        /* set<int> myset;
+        for (int i = 0; i < n; i++) {
+            if (myset.find(A[i]) == myset.end()) {
+                myset.insert(A[i]);
+            } else {
+                myset.erase(A[i]);
+            }
+        }
+        return *myset.begin();
+        */
         int result = 0;
         int i = 0;
         while (i < n) {
