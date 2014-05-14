435945e6f0ffeae7b9fb0fc304e054395f77893a
jnozsc <jnozsc@gmail.com>
2014-05-14 11:37:54 -0400

    use dp

--- Unique_Binary_Search_Trees.cpp
+++ Unique_Binary_Search_Trees.cpp
@@ -1,10 +1,17 @@
+// dp
+// num[i] = for loop( sum(num[0...k] x num[k...i]))
+
 class Solution {
   public:
     int numTrees(int n) {
-        int result = 1;
-        for (int i = 2; i <= n; i++) {
-            result = 2 * (2 * i - 1) * result / (i + 1);
+        vector<int> num(n + 1, 0);
+        num[0] = 1;
+        num[1] = 1;
+        for (int i = 2 ; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                num[i] += num[j] * num[i - j - 1];
+            }
         }
-        return result;
+        return num[n];
     }
 };
