b37ba115d25dfe8ce5518910c0e96d2b174c1779
jnozsc <jnozsc@gmail.com>
2014-05-18 08:43:45 -0400

    change style

--- ZigZag_Conversion.cpp
+++ ZigZag_Conversion.cpp
@@ -1,7 +1,9 @@
 class Solution {
   public:
     string convert(string s, int nRows) {
-        if (nRows <= 1 || nRows >= s.length()) return s;
+        if (nRows <= 1 || nRows >= s.length()) {
+            return s;
+        }
         vector<string> result(nRows);
         string resultString = "";
         int currentRow = 0;
@@ -10,10 +12,17 @@ class Solution {
         while (index < s.length()) {
             result[currentRow] = result[currentRow] + s.substr(index, 1);
             index++;
-            if (currentRow == 0)currentRowRaise = true;
-            if (currentRow == nRows - 1)currentRowRaise = false;
-            if (currentRowRaise) currentRow++;
-            else currentRow--;
+            if (currentRow == 0) {
+                currentRowRaise = true;
+            }
+            if (currentRow == nRows - 1) {
+                currentRowRaise = false;
+            }
+            if (currentRowRaise) {
+                currentRow++;
+            } else {
+                currentRow--;
+            }
         }
         for (int j = 0; j < result.size(); j++) {
             resultString += result[j];
