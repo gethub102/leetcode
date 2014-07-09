fd75449e5bb2f20e1dd30d0b716a1334daad9f93
jnozsc <jnozsc@gmail.com>
2014-07-09 11:14:31 -0400

    use dp

--- Edit_Distance.cpp
+++ Edit_Distance.cpp
@@ -1,6 +1,15 @@
+/*
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+*/
+// first, stupid
 class Solution {
   public:
-//这个题目解的太崎岖了。
 //首先这个题目是个定律 叫作 Levenshtein distance，这个算法是这样的
 // (1)str1或str2的长度为0返回另一个字符串的长度。 if(str1.length==0) return str2.length; if(str2.length==0) return str1.length;
 // (2)初始化(n+1)*(m+1)的矩阵d，并让第一行和列的值从0开始增长。
@@ -32,5 +41,35 @@ class Solution {
 
         }
         return dp[length1 - 1][length2 - 1];
+    }
+};
+
+// second dp
+class Solution {
+  public:
+    int minDistance(string word1, string word2) {
+        if (word1.empty()) {
+            return word2.length();
+        }
+        if (word2.empty()) {
+            return word1.length();
+        }
+        vector<vector<int>> dp(word1.length() + 1, vector<int>(word2.length() + 1, 0));
+        for (int i = 0; i <= word2.length(); i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i <= word1.length(); i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
     }
 };
