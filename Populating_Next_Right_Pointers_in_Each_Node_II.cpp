8c28374161455798ed5794479ebbd1b2761f46d6
jnozsc <jnozsc@gmail.com>
2014-07-11 12:45:38 -0400

    rewrite

--- Populating_Next_Right_Pointers_in_Each_Node_II.cpp
+++ Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -1,3 +1,25 @@
+/*
+Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+*/
 /**
  * Definition for binary tree with next pointer.
  * struct TreeLinkNode {
@@ -9,25 +31,29 @@
 class Solution {
   public:
     void connect(TreeLinkNode* root) {
-        if (!root) return;
-        TreeLinkNode* p = root->next;
-        while (p) {
-            if (p->left) {
-                p = p->left;
+        if (!root) {
+            return;
+        }
+        // assume we have build this level's next, now work on next level
+        TreeLinkNode* current = root->next;
+        while (current) {
+            if (current->left) {
+                current = current->left;
                 break;
             }
-            if (p->right) {
-                p = p->right;
+            if (current->right) {
+                current = current->right;
                 break;
             }
-            p = p->next;
+            current = current->next;
         }
         if (root->right) {
-            root->right->next = p;
+            root->right->next = current;
         }
         if (root->left) {
-            root->left->next = root->right ? root->right : p;
+            root->left->next = root->right ? root->right : current;
         }
+        // first right, then left!!!
         connect(root->right);
         connect(root->left);
     }
