837345939a01ade81ae1b6659f93b97b9405514f
jnozsc <jnozsc@gmail.com>
2014-07-14 21:17:17 -0400

    rewrite use recursion

--- Clone_Graph.cpp
+++ Clone_Graph.cpp
@@ -1,3 +1,27 @@
+/*
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+*/
 /**
  * Definition for undirected graph.
  * struct UndirectedGraphNode {
@@ -6,6 +30,8 @@
  *     UndirectedGraphNode(int x) : label(x) {};
  * };
  */
+
+// version 1, use a queue
 class Solution {
   public:
     UndirectedGraphNode* cloneGraph(UndirectedGraphNode* node) {
@@ -44,5 +70,40 @@ class Solution {
             }
         }
         return nodeCopy;
+    }
+};
+
+// version 2, recursion
+/*
+At the first look, this problem seems wired. Remind the Copy Constructor of a class object, when the class member contains pointers, the direct copy is shadow, we need deep copy. For this problem, there are two major tasks:
+(1)  Traverse the graph
+(2)  Construct the new graph at the same time.
+
+Traverse the graph is similar to the tree traversal, both DFS and  BFS can be used. Slight difference  is to consider the loop in the graph (e.g., the 2-->2 in the above figure), thus we need store the visited vertex information. If the neighbor of current node has not been visited, then search that node.
+
+Only one point should be noticed for constructing the new graph, in the code below, I used a map< int, node*> to store the visited information, so that if the node has been visited, just add the map[int] to the current node's neighbor list. Be careful the current node is the node you "newed", but not the node in the original graph (line 98 and 99 in the code below. If you set visited[v->label] = v, it is not correct.).
+*/
+class Solution {
+  public:
+    UndirectedGraphNode* cloneGraph(UndirectedGraphNode* node) {
+        if (!node) {
+            return NULL;
+        } else {
+            map<int, UndirectedGraphNode*> visited;
+            return dfs(node, visited);
+        }
+    }
+
+    UndirectedGraphNode* dfs(UndirectedGraphNode* node, map<int, UndirectedGraphNode*>& visited) {
+        UndirectedGraphNode* result = new UndirectedGraphNode(node->label);
+        visited[node->label] = result;
+        for (int i = 0; i < node->neighbors.size(); i++) {
+            if (!visited[node->neighbors[i]->label]) {
+                result->neighbors.push_back(dfs(node->neighbors[i], visited));
+            } else {
+                result->neighbors.push_back(visited[node->neighbors[i]->label]);
+            }
+        }
+        return result;
     }
 };
