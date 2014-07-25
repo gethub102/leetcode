0d0fd85ac26177916799b5e37e1a2f4459ebf591
jnozsc <jnozsc@gmail.com>
2014-07-25 12:46:35 -0400

    rewrite

--- Anagrams.cpp
+++ Anagrams.cpp
@@ -1,20 +1,29 @@
+/*
+Given an array of strings, return all groups of strings that are anagrams.
+
+Note: All inputs will be in lower-case.
+*/
 class Solution {
   public:
     vector<string> anagrams(vector<string>& strs) {
         vector<string> result;
-        if (strs.size() <= 1) return result;
-        map<string, int> anagram;
+        if (strs.size() <= 1) {
+            return result;
+        }
+        map<string, int> myMap;
         for (int i = 0; i < strs.size(); i++) {
-            string s = strs[i];
-            sort(s.begin(), s.end());
-            if (anagram.find(s) == anagram.end()) {
-                anagram.insert(make_pair(s, i));
-            } else {
-                if (anagram[s] >= 0) {
-                    result.push_back(strs[anagram[s]]);
-                    anagram[s] = -1;
+            string current = strs[i];
+            sort(current.begin(), current.end());
+            if (myMap.find(current) != myMap.end()) {
+                if (myMap[current] != -1) {
+                    result.push_back(strs[myMap[current]]);
+                    result.push_back(strs[i]);
+                    myMap[current] = -1;
+                } else {
+                    result.push_back(strs[i]);
                 }
-                result.push_back(strs[i]);
+            } else {
+                myMap[current] = i;
             }
         }
         return result;
