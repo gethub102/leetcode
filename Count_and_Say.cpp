095ecf01ec3236e846bcbfdeeea5ec6631e28d88
jnozsc <jnozsc@gmail.com>
2014-07-27 09:07:00 -0400

    rewrite use recursion

--- Count_and_Say.cpp
+++ Count_and_Say.cpp
@@ -1,31 +1,30 @@
+/*
+The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string.
+*/
 class Solution {
   public:
     string countAndSay(int n) {
-        if (n == 1) return "1";
-        int times = 1;
-        string result = "1";
-        while (times < n) {
-            vector<string> stringV;
-            vector<int> countV;
-            int index = 0;
-            while (index < result.length()) {
-                if (stringV.empty() || stringV.back() != result.substr(index, 1)) {
-                    stringV.push_back(result.substr(index, 1));
-                    countV.push_back(1);
-                } else {
-                    countV[countV.size() - 1] ++;
-                }
-                index++;
-            }
-            result = "";
-            while (!stringV.empty()) {
-                result = stringV.back() + result;
-                result = to_string(countV.back()) + result;
-                stringV.pop_back();
-                countV.pop_back();
-            }
-            times++;
+        if (n < 2) {
+            return "1";
         }
-        return result;
+        string s = countAndSay(n - 1);
+        int count = 1;
+        string say = "";
+        for (int i = 1; i < s.size(); i++, count++) {
+            if (s[i] != s[i - 1]) {
+                say += to_string(count) + s[i - 1];
+                count = 0;
+            }
+        }
+        say += to_string(count) + s.back();
+        return say;
     }
 };
