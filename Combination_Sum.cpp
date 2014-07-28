38cd0252bbe53a8fd03128eb30fa3e46fb652cb1
jnozsc <jnozsc@gmail.com>
2014-07-28 09:18:20 -0400

    rewrite

--- Combination_Sum.cpp
+++ Combination_Sum.cpp
@@ -1,23 +1,37 @@
+/*
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7,
+A solution set is:
+[7]
+[2, 2, 3]
+*/
 class Solution {
   public:
     vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int> > results;
-        vector<int> singleSolution;
+        vector<vector<int> > result;
+        vector<int> oneSolution;
         sort(candidates.begin(), candidates.end());
-        combinationSumRecusion(results, singleSolution, candidates, target);
-        return results;
+        combinationSumDFS(result, oneSolution, candidates, target);
+        return result;
     }
 
-    void combinationSumRecusion(vector<vector<int> >& results, vector<int> singleSolution, vector<int> candidates, int target) {
-        if (target == 0) {
-            results.push_back(singleSolution);
+    void combinationSumDFS(vector<vector<int>>& result, vector<int> oneSolution, vector<int>& candidates, int target) {
+        if (target == 0 && !oneSolution.empty()) {
+            result.push_back(oneSolution);
             return;
         }
         for (int i = 0; i < candidates.size(); i++) {
-            if (singleSolution.empty() || (target - candidates[i] >= 0 && candidates[i] >= singleSolution.back())) {
-                singleSolution.push_back(candidates[i]);
-                combinationSumRecusion(results, singleSolution, candidates, target - candidates[i]);
-                singleSolution.pop_back();
+            if (candidates[i] <= target && (oneSolution.empty() || candidates[i] >= oneSolution.back())) {
+                oneSolution.push_back(candidates[i]);
+                combinationSumDFS(result, oneSolution, candidates, target - candidates[i]);
+                oneSolution.pop_back();
             }
         }
     }
