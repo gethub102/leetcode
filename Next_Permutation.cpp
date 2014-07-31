09512b42c7f3f9ba718e8ed69815169d182b603c
jnozsc <jnozsc@gmail.com>
2014-07-31 14:19:35 -0400

    rewrite

--- Next_Permutation.cpp
+++ Next_Permutation.cpp
@@ -4,14 +4,18 @@ class Solution {
         assert(num.size() > 0);
         int index = num.size() - 1;
         while (index > 0) {
-            if (num[index - 1] < num[index])break;
+            if (num[index - 1] < num[index]) {
+                break;
+            }
             index --;
         }
         if (index > 0) {
             int i = index - 1;
             int j = num.size() - 1;
             while (j > i) {
-                if (num[j] > num[i]) break;
+                if (num[j] > num[i]) {
+                    break;
+                }
                 j--;
             }
             swap(num[i], num[j]);
