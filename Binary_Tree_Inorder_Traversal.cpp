d9b90a5c1abda0e10040bb6ba88f72c2cbaacdb9
jnozsc <jnozsc@gmail.com>
2014-08-01 21:06:43 -0400

    add more solutions

--- Binary_Tree_Inorder_Traversal.cpp
+++ Binary_Tree_Inorder_Traversal.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+//version 1 recursion
 class Solution {
   public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -16,9 +18,84 @@ class Solution {
     }
 
     void inorderTraversalRecursion(TreeNode* root, vector<int>& result) {
-        if (!root)return;
+        if (!root) {
+            return;
+        }
         inorderTraversalRecursion(root->left, result);
         result.push_back(root->val);
         inorderTraversalRecursion(root->right, result);
+    }
+};
+
+// version 2 iterator
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> myStack;
+        TreeNode* current = root;
+        while (current || !myStack.empty()) {
+            if (current) {
+                myStack.push(current->right);
+                myStack.push(current);
+                current = current->left;
+            } else {
+                result.push_back(myStack.top()->val);
+                myStack.pop();
+                current = myStack.top();
+                myStack.pop();
+            }
+        }
+        return result;
+    }
+};
+
+// version 3 morris
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (!root) {
+            return result;
+        }
+        TreeNode* current = root, *prev = NULL;
+        while (current) {
+            if (!current->left) {
+                result.push_back(current->val);
+                current = current->right;
+            } else {
+                prev = current->left;
+                while (prev->right && prev->right != current) {
+                    prev = prev->right;
+                }
+                if (!prev->right) {
+                    prev->right = current;
+                    current = current->left;
+                } else {
+                    prev->right = NULL;
+                    result.push_back(current->val);
+                    current = current->right;
+                }
+            }
+        }
+        return result;
     }
 };
