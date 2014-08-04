732acc0f6a8101c6dd77df9145192c69ce20d2d9
jnozsc <jnozsc@gmail.com>
2014-08-04 10:39:07 -0400

    change style

--- Decode_Ways.cpp
+++ Decode_Ways.cpp
@@ -1,15 +1,19 @@
 class Solution {
   public:
     int numDecodings(string s) {
-        if (s.empty()) return 0;
+        if (s.empty()) {
+            return 0;
+        }
         vector<int> ways(s.size() + 1, 1);
         for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '0')
+            if (s[i] == '0') {
                 ways[i] = 0;
-            else
+            } else {
                 ways[i] = ways[i + 1];
-            if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+            }
+            if (i + 1 < s.size() && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))) {
                 ways[i] += ways[i + 2];
+            }
         }
         return ways[0];
     }
