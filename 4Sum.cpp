b3fd914da12dde31cebd6e58ed37ae68881e6ae6
jnozsc <jnozsc@gmail.com>
2014-08-11 15:59:19 -0400

    add question

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,8 +1,23 @@
+/*
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+Note:
+Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+The solution set must not contain duplicate quadruplets.
+    For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+
+    A solution set is:
+    (-1,  0, 0, 1)
+    (-2, -1, 1, 2)
+    (-2,  0, 0, 2)
+*/
 class Solution {
   public:
     vector<vector<int> > fourSum(vector<int>& num, int target) {
         vector<vector<int> >  result;
-        if (num.size() < 4) return result;
+        if (num.size() < 4) {
+            return result;
+        }
         sort(num.begin(), num.end());
         for (int i = 0; i <= num.size() - 4; i++) {
             //    if(i>0 && num[i-1]==num[i-1]) continue;
