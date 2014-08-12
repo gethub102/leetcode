3d1a725ad85cd67974dc063eed6a611174f7ce57
jnozsc <jnozsc@gmail.com>
2014-08-12 14:40:04 -0400

    new question

--- Palindrome_Number.cpp
+++ Palindrome_Number.cpp
@@ -1,8 +1,24 @@
+/*
+Determine whether an integer is a palindrome. Do this without extra space.
+
+Some hints:
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+*/
 class Solution {
   public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
-        if (x >= 0 && x <= 9) return true;
+        if (x < 0) {
+            return false;
+        }
+        if (x >= 0 && x <= 9) {
+            return true;
+        }
         int n = x;
         int rev = 0;
         while (x > 0) {
