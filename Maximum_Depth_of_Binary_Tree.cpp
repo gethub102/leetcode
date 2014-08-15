9df0296a795f193cd5d69c4e77fbeb088dbbe893
jnozsc <jnozsc@gmail.com>
2014-08-15 16:46:37 -0400

    rewrite

--- Maximum_Depth_of_Binary_Tree.cpp
+++ Maximum_Depth_of_Binary_Tree.cpp
@@ -1,3 +1,8 @@
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -7,6 +12,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// first one
 class Solution {
   public:
     int maxDepth(TreeNode* root) {
@@ -18,5 +25,44 @@ class Solution {
             return level;
         }
         return max(maxDepthRecursion(root->left, level + 1), maxDepthRecursion(root->right, level + 1));
+    }
+};
+
+// second, recursion rewrite
+
+class Solution {
+  public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+class Solution {
+  public:
+    int maxDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        queue<TreeNode*> myQueue;
+        myQueue.push(root);
+        int result = 0;
+        while (!myQueue.empty()) {
+            int size = myQueue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* current  = myQueue.front();
+                myQueue.pop();
+                if (current->left) {
+                    myQueue.push(current->left);
+                }
+                if (current->right) {
+                    myQueue.push(current->right);
+                }
+            }
+            result++;
+        }
+        return result;
     }
 };
