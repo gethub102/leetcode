e8fd9316fef3d19404c56c287b1708affd109833
jnozsc <jnozsc@gmail.com>
2014-08-16 12:59:56 -0400

    rewrite

--- Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,3 +1,9 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+*/
+
 class Solution {
   public:
     int maxProfit(vector<int>& prices) {
@@ -10,5 +16,23 @@ class Solution {
             max_value = max(max_value, prices[i]);
         }
         return max_profit;
+    }
+};
+
+// rewrite
+
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) {
+            return 0;   // DON'T forget it
+        }
+        int currentMax = prices.back();
+        int result = 0;
+        for (int i = prices.size() - 2; i >= 0; i--) {
+            result = max(result, currentMax - prices[i]);
+            currentMax = max(currentMax, prices[i]);
+        }
+        return result;
     }
 };
