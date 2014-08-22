6864a633185c63d169abee14536621c95620b50e
jnozsc <jnozsc@gmail.com>
2014-08-22 07:53:31 -0700

    rewrite

--- Linked_List_Cycle_II.cpp
+++ Linked_List_Cycle_II.cpp
@@ -1,3 +1,9 @@
+/*
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Follow up:
+Can you solve it without using extra space?
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -27,5 +33,42 @@ class Solution {
             fast = fast->next;
         }
         return fast;
+    }
+};
+
+// rewrite
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode* detectCycle(ListNode* head) {
+        if (!head || !head->next) {
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (!fast || !fast->next) {
+            return NULL;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
 };
