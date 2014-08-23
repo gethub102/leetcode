8dbf5b75dc4f6eef7583c8a23d4b17be7e181380
jnozsc <jnozsc@gmail.com>
2014-08-23 09:18:03 -0400

    rewrite

--- Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring.cpp
@@ -1,3 +1,6 @@
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
 /* 这个题的坑爹之处在于正常i从0~last, j从0~last那么循环不行。
 * 因为d[i][j]用到了d[i + 1][j - 1]，说明i要从后往前，j则要从前往后。导致二层循环是这种顺序：
 * for (int i = last; i >= 0; i–)
@@ -35,6 +38,42 @@ class Solution {
                     end = j;
                 }
             }
+        }
+        return s.substr(start, end - start + 1);
+    }
+};
+
+// rewrite
+
+class Solution {
+  public:
+    string longestPalindrome(string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        bool dp[s.length()][s.length()];
+        int result = 1;
+        int start = 0;
+        int end = 0;
+        for (int i = s.length() - 1 ; i >= 0; i--) {
+            for (int j = i; j <= s.length() - 1; j ++) {
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (j == i + 1) {
+                    dp[i][j] = s[i] == s[j];
+                } else {
+                    dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1];
+                }
+
+                if (dp[i][j]) {
+                    if (j - i + 1 > result) {
+                        result = j - i + 1;
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+
         }
         return s.substr(start, end - start + 1);
     }
