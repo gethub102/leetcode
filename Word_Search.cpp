17b85d81ac75e5d67b4812903fedf1c3f2afacc7
jnozsc <jnozsc@gmail.com>
2014-09-03 16:31:04 -0400

    add question

--- Word_Search.cpp
+++ Word_Search.cpp
@@ -1,3 +1,17 @@
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+For example,
+Given board =
+[
+  ["ABCE"],
+  ["SFCS"],
+  ["ADEE"]
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+*/
 class Solution {
   public:
     // 本来一道很简单的 dfs，按照惯性又用了 void，直接 bool return 多快
