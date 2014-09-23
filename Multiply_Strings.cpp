caa65a54801132aa34a9acf9b66844b9eb871b90
jnozsc <jnozsc@gmail.com>
2014-09-23 10:13:47 -0400

    add question

--- Multiply_Strings.cpp
+++ Multiply_Strings.cpp
@@ -1,3 +1,7 @@
+/*
+Given two numbers represented as strings, return multiplication of the numbers as a string.
+Note: The numbers can be arbitrarily large and are non-negative.
+*/
 class Solution {
   public:
     string multiply(string num1, string num2) {
