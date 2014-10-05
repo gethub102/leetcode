974fad9294c8afd0aa0092b4839924e2e333921f
jnozsc <jnozsc@gmail.com>
2014-10-05 07:32:57 -0400

    add question

--- Pascal_s_Triangle_II.cpp
+++ Pascal_s_Triangle_II.cpp
@@ -1,3 +1,19 @@
+/*
+Given an index k, return the kth row of the Pascal's triangle.
+For example, given k = 3,
+Return [1,3,3,1].
+
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+*/
 class Solution {
   public:
     vector<int> getRow(int rowIndex) {
